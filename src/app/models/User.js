397bc6683847225523a164fef79e16de015ba2ef
matheuskaio <matheuskaio@github.com>
2019-10-27 22:50:18 -0300

    adicionando o avatar

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -23,6 +23,11 @@ class User extends Model {
     return this;
   }
 
+  static associate(models) {
+    // o belongsTo diz que a teremos uma chave estrangeira de FILE em USER
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -13,7 +14,13 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+    // na linha abaixo utilizada para fazer o relacionamento entre FILE e USER
+    // onde no model => model.associate estamos verificado para cada model se
+    // ele possui essa função e model.associate(this.connection.models) é onde
+    // estamos utilizando a função
   }
 }
 
